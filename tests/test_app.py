a9c6f8901fc9f9d624d46defad1b6adf4467918e
JC <>
2025-11-05 21:17:01 +0500

    🎨 Improved formatting:

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,28 +1,40 @@
 import pandas as pd
 
+DS_NAME = """https://huggingface.co/datasets/ankislyakov/titanic/resolve/main/titanic_train.csv"""
+
+
 def test_df_columns_exist():
     """Dataframes columns exists"""
-    titanic = pd.read_csv(
-        'https://huggingface.co/datasets/ankislyakov/titanic/resolve/main/titanic_train.csv',
-        index_col='PassengerId'
+    titanic = pd.read_csv(DS_NAME, index_col="PassengerId")
+    assert all(
+        x
+        for x in set(titanic.columns)
+        if x
+        in {
+            "Survived",
+            "Pclass",
+            "Name",
+            "Sex",
+            "Age",
+            "SibSp",
+            "Parch",
+            "Ticket",
+            "Fare",
+            "Cabin",
+            "Embarked",
+        }
     )
-    assert all(x for x in set(titanic.columns) if x in {'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'})
-    
+
+
 def test_df_embarked_values():
     """Valid embarked values"""
-    titanic = pd.read_csv(
-        'https://huggingface.co/datasets/ankislyakov/titanic/resolve/main/titanic_train.csv',
-        index_col='PassengerId'
-    )
-    embarked_values = set(titanic['Embarked'].dropna().unique())
+    titanic = pd.read_csv(DS_NAME, index_col="PassengerId")
+    embarked_values = set(titanic["Embarked"].dropna().unique())
 
-    assert embarked_values.issubset({'C', 'Q', 'S'})
-    
+    assert embarked_values.issubset({"C", "Q", "S"})
+
+
 def test_dataframe_len():
     """Test dataframe lenght"""
-    titanic = pd.read_csv(
-        'https://huggingface.co/datasets/ankislyakov/titanic/resolve/main/titanic_train.csv',
-        index_col='PassengerId'
-    )
+    titanic = pd.read_csv(DS_NAME, index_col="PassengerId")
     assert len(titanic) == 891
-
